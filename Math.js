befee66c59e1ef29e575d96e6bd1b012d0f9ad45
Elizabeth Alexander <elizabethalexander189@gmail.com>
2020-05-27 20:31:34 +0530

    Basics - Math Object

---
+++ Math.js
@@ -0,0 +1,20 @@
+//JavaScript Math Object allows to perform mathematical tasks on nums
+console.log(Math.PI); //Returns 3.141592653589793
+console.log(Math.round(4.7)); //Math.round(x) returns the value of x rounded to its nearest integer; Result is 5
+console.log(Math.round(4.4)); //Result is 4
+console.log(Math.pow(8, 2)); //Math.pow(x, y) returns the value of x to the power of y; Result is 64
+console.log(Math.sqrt(64)); //Math.sqrt(x) returns the square root of x; Result is 8
+console.log(Math.abs(-4.7));//Math.abs(x) returns the absolute (positive) value of x; Result is 4.7
+console.log(Math.ceil(4.4)); //Math.ceil(x) returns the value of x rounded up to its nearest integer; Result is 5
+console.log(Math.floor(4.7)); //Math.floor(x) returns the value of x rounded down to its nearest integer; Result is 4
+console.log(Math.sin(90 * Math.PI/180)); //returns 1 (the sine of 90 degrees);
+/* Math.sin(x) returns the sine (a value between -1 and 1) of the angle x (given in radians).
+
+If you want to use degrees instead of radians, you have to convert degrees to radians:
+
+Angle in radians = Angle in degrees x PI / 180. */
+console.log(Math.cos(0 * Math. PI / 180)); // returns 1 (the cos of 0 degrees)
+console.log(Math.min(0, 150, 30, 20, -8, -200)); //Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments; Result is -200
+console.log(Math.max(0, 150, 30, 20, -8, -200)); //Result is 150
+console.log(Math.random());
+
